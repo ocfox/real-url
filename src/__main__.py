e40f251767623531604541a84f4f8b7371c4e507
ocfox <i@ocfox.me>
2022-08-24 14:31:36 +0800

    use main and init

---
+++ src/__main__.py
@@ -0,0 +1,20 @@
+import bilibili
+import douyu
+import huya
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("platform", help="Live platform e.g. douyu")
+    parser.add_argument("room", help="room id")
+    args = parser.parse_args()
+
+    match args.platform:
+        case "douyu":
+            print(douyu.DouYu(args.room).get_real_url())
+        case "bilibili":
+            print(bilibili.get_real_url(args.room))
+        case "huya":
+            print(huya.get_real_url(args.room))
+        case _:
+            print(f"There's no platform called {args.platform}, please check your input.")
