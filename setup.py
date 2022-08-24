19b4dc12129789688a78fba08f2915e90d3418ab
ocfox <i@ocfox.me>
2022-08-24 14:06:12 +0800

    fix typo

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="sampleproject",
+    name="real-url",
     version="0.1.0",
     description="A live url get tool",
     package_dir={"": "src"},
